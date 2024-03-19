9657fca04d065437730de9b4ddad0081b5082dc7
benfar <benoit.farine@gmail.com>
2024-03-18 21:44:17 -0400

    Update 01_01_available_books.js

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,2 +1,7 @@
-// Write your code here
-console.log("Hello");
+async getUsers(){
+  let results = await fetch('https://randomuser.me/api/?results=5');
+  let users = await results.json();
+  console.log(users);
+}
+
+getUsers();
